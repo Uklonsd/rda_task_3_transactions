f0c3e7d70f7c4c1674fc8fd2ccae98fa6d693be2
Uklonsd <khubashvili12@gmail.com>
2025-02-11 19:56:36 +0200

    Fix!

--- task.sql
+++ task.sql
@@ -4,11 +4,12 @@ START TRANSACTION;
 INSERT INTO Orders (CustomerID, Date)
 VALUES (1, '2023-01-01');
 
+SET @LastOrderID = LAST_INSERT_ID();
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (1, 1, 1);
+VALUES (@LastOrderID, 1, 1);
 
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
-WHERE ID = 1 AND WarehouseAmount >= 1;  -- Ensure there's enough stock
+WHERE ID = 1 AND WarehouseAmount >= 1;
 COMMIT;
