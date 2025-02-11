afa95d85196323d67409a3e07d4d61bb4683694b
Uklonsd <khubashvili12@gmail.com>
2025-02-11 19:50:07 +0200

    Fix!

--- task.sql
+++ task.sql
@@ -1,12 +1,14 @@
 USE ShopDB;
 
+START TRANSACTION;
 INSERT INTO Orders (CustomerID, Date)
-SELECT 1, '2023-01-01'
-WHERE NOT EXISTS (SELECT 1 FROM Orders WHERE ID = 1);
+VALUES (1, '2023-01-01');
+
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-VALUES (1, 1, 5);
+VALUES (1, 1, 1);
 
-START TRANSACTION;
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1 AND WarehouseAmount >= 1;  -- Ensure there's enough stock
 COMMIT;
